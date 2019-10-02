47feac21ae109bdf55fc7a5f13a96c77faa53160
Yuchen Li <ychnlgy@gmail.com>
2019-10-01 20:43:48 -0400

    Added unet classifier

--- examples/classify_cifar10_unet.py
+++ examples/classify_cifar10_unet.py
@@ -3,5 +3,18 @@ import torch
 import unet_cifar10
 
 
-class Classifier:
+class Classifier(torch.nn.Module):
     
+    def __init__(self, unet, tune, tail):
+        super().__init__()
+        self.unet = unet
+        self.tune = tune
+        self.tail = tail
+    
+    def forward(self, X):
+        if tune:
+            X = self.unet(X)
+        else:
+            with torch.no_grad():
+                X = self.unet(X).clone().detach()
+        return self.tail(X)
