01d2b6c13cf1a6b5d46103c2314f90eee0b95e99
Yuchen Li <ychnlgy@gmail.com>
2019-10-27 18:50:51 -0400

    Debug tensor version of depth cutout

--- examples/visual_mvor.py
+++ examples/visual_mvor.py
@@ -55,6 +55,7 @@ def main(args):
                                     assert args.num_image_samples == 0
                                     #xh_box = cutout_human_tensor(test_data, i, xh_box, args.cut_thickness, conv) > 0
                                     xh_box = cutout_human(test_data, i, xh_box, args.cut_thickness, args.filter_size, args.threshold)
+                                    xh_box = xh_box.to(device)
                                 intersection = xh_box.float() * y
                                 intersection_sum = intersection.sum()
                                 union_sum = xh_box.sum() + y.sum() - intersection_sum
