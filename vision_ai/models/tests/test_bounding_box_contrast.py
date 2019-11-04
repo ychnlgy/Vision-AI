a6aacec41a82a2cdb73235a4425af2b8bee20253
Yuchen Li <ychnlgy@gmail.com>
2019-11-04 14:01:36 -0500

    Added test for bounding box contrast loss

--- vision_ai/models/tests/test_bounding_box_contrast.py
+++ vision_ai/models/tests/test_bounding_box_contrast.py
@@ -43,3 +43,28 @@ def test_one_box_same():
     ) - (-1.0)).abs() < eps
 
 
+def test_one_box_diff():
+    bbox_coords = [
+        [(0, 0, 2, 2)],
+        [(0, 0, 2, 2)]
+    ]
+
+    pred_xh = torch.zeros(2, 2, 4, 4)
+    embeddings_xh = torch.zeros(2, 3, 4, 4)
+    for i, bboxes in enumerate(bbox_coords):
+        x, y, w, h = bboxes[0]
+        pred_xh[i, 1, x:x+w, y:y+h] = 1
+        for dx in range(w):
+            for dy in range(h):
+                s = int((dx + dy) > 1)
+                embeddings_xh[i, :, x+dx, y+dy] = torch.Tensor(
+                    [dx*(1-s), dy*(1-s), s]
+                )
+
+    eps = 1e-6
+    assert (vision_ai.models.bounding_box_contrast.batch_loss(
+        pred_xh,
+        embeddings_xh,
+        bbox_coords,
+        frac_compare=1.0
+    ) - 0.0).abs() < eps
